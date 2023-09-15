6c7d8a8b2cf3ade0cf6ed38db7f5ae28b4ca7a51
Mochammad Shenna Wardana <shenawardana@gmail.com>
2023-09-15 08:57:30 +0700

    fix: remove unnecessary code

--- server/app/database_test.go
+++ server/app/database_test.go
@@ -2,14 +2,24 @@ package app
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"testing"
 
 	_ "github.com/lib/pq"
+	config "github.com/mochammadshenna/aplikasi-po/configs"
 )
 
 func TestOpenConnection(t *testing.T) {
-	connStr := "host=localhost port=5432 user=root  password=root dbname=arbrion"
+	var dbConfig = config.Get().Database
+
+	connStr := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s",
+		dbConfig.Host,
+		dbConfig.Port,
+		dbConfig.Username,
+		dbConfig.Password,
+		dbConfig.DbName,
+	)
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
